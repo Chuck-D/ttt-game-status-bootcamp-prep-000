a8134aa2810732081cf6ab8892120604cf7fe015
Adrian Davis <chuck.d.kc@gmail.com>
2018-05-03 23:49:54 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -56,7 +56,7 @@ end
 def winner(board)
     if won?(board)
         winning_player=won?(board).first
-        return board[winning_player]      
+        #return board[winning_player]      
     end
 end
     
